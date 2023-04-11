2d621370d09e985349a7a46e5fe8333722d56e46
Viktor <tsukanov87@mail.ru>
2023-04-11 15:53:32 +0300

    8 directional sprite animation

--- script.js
+++ script.js
@@ -16,17 +16,34 @@ window.addEventListener('load', function () {
 	class Player {
 		constructor(game) {
 			this.game = game;
-			//извлекаем из game свойства и присваиваем коллизии
+			//извлекаем из game свойства и присваиваем коллизии центр круга героя
 			this.collisionX = this.game.width * 0.5;
 			this.collisionY = this.game.height * 0.5;
 			this.collisionRadius = 30;
 			//устанавливаем начальную скорость игрока
 			this.speedX = 0;
 			this.speedY = 0;
+
+			this.dx = 0;
+			this.dy = 0;
+
 			this.speedModifier = 5;
+			this.spriteWidth = 255;
+			this.spriteHeight = 255;
+			this.width = this.spriteWidth;
+			this.height = this.spriteHeight;
+			// верхний левй угол картинки игрока
+			this.spriteX;
+			this.spriteY;
+
+			this.frameX = 0;
+			this.frameY = 0;
+
+			this.image = document.getElementById('bull');
 		}
 		//рисует игрок
 		draw(context) {
+			context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
 			//запускает новый путь, очищая список вложенных путей.
 			context.beginPath();
 			//Метод Canvas 2D API добавляет дугу окружности к текущему подпути.
@@ -48,11 +65,31 @@ window.addEventListener('load', function () {
 
 		}
 		update() {
+
+			//АНИМИРУЕМ ПЕРСОНАЖА
+
 			//движение линии за объектом
 			//ось x
 			this.dx = this.game.mouse.x - this.collisionX;
 			//ось y
 			this.dy = this.game.mouse.y - this.collisionY;
+			//Метод Math.atan2() возвращает арктангенс от частного своих аргументов.
+			const angle = Math.atan2(this.dy, this.dx);
+
+			if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+			else if (angle < -1.96) this.frameY = 7;
+			else if (angle < -1.17) this.frameY = 0;
+			else if (angle < -0.39) this.frameY = 1;
+			else if (angle < 0.39) this.frameY = 2;
+			else if (angle < 1.17) this.frameY = 3;
+			else if (angle < 1.96) this.frameY = 4;
+			else if (angle < 2.74) this.frameY = 5;
+
+
+			console.log(angle);
+
+
+
 			//постоянная скорость героя
 			//высчитываеи гипотенузу
 			const distance = Math.hypot(this.dy, this.dx);
@@ -70,6 +107,8 @@ window.addEventListener('load', function () {
 
 			this.collisionX += this.speedX * this.speedModifier;
 			this.collisionY += this.speedY * this.speedModifier;
+			this.spriteX = this.collisionX - this.width * 0.5;
+			this.spriteY = this.collisionY - this.height * 0.5 - 100;
 
 			//берём массив из obstacles перебираем его в функции obstacle
 			//выводим в консоль метод checkCollision
